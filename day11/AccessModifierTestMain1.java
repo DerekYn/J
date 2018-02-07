79a8424a89eb3a49581127f7f3e98b86ff14b833
DerekYn <33822447+DerekYn@users.noreply.github.com>
2018-02-07 17:21:57 +0900

    Add files via upload

---
+++ day11/AccessModifierTestMain1.java
@@ -0,0 +1,16 @@
+package my.day11;
+
+public class AccessModifierTestMain1 {
+
+	public static void main(String[] args) {
+		
+		AccessModifierTest obj = new AccessModifierTest();
+		
+		System.out.println("사용자 ID : " + obj.userid);
+		System.out.println("암호 : " + obj.passwd);
+		System.out.println("이름 : " + obj.name);
+		
+		
+	}
+
+}
