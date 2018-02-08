b3e80b2c9d4c4fe61cc818e6c1929999bfa16b4f
DerekYn <33822447+DerekYn@users.noreply.github.com>
2018-02-08 20:57:38 +0900

    NewStudent

---
+++ day11/Student.java
@@ -0,0 +1,29 @@
+package my.day11;
+
+public class Student {
+	
+	private String numberOfStudent = "";
+	private String name = "";
+	private String mobile = "";
+	
+	public Student() { }
+	
+	public String getNumberOfStudent() {
+		return numberOfStudent;
+	}
+	public void setNumberOfStudent(String numberOfStudent) {
+		this.numberOfStudent = numberOfStudent;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+}
