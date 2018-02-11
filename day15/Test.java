b314149207503ac4f294e682c7e387b54d33112c
DerekYn <33822447+DerekYn@users.noreply.github.com>
2018-02-11 10:34:18 +0900

    Add files via upload

---
+++ day15/Test.java
@@ -0,0 +1,16 @@
+package my.day15;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		int r = 2;
+		final float fi = 3.14159F;	// fianl 변수를 상수변수라고 한다.
+		
+		System.out.println("원 둘레 => " + 2*fi*r);
+		
+		// fi = 0;
+		System.out.println("원 둘레 => " + 2*fi*r);
+		
+	}
+
+}
