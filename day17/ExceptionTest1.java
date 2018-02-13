c8d6c38019dbd7681f9253d309f775b705a4180f
DerekYn <33822447+DerekYn@users.noreply.github.com>
2018-02-13 15:38:15 +0900

    Add files via upload

---
+++ day17/ExceptionTest1.java
@@ -0,0 +1,19 @@
+package my.day17;
+
+public class ExceptionTest1 {
+
+	public static void main(String[] args) {
+		
+		
+		String[] subjectArr = {"Java", "Oracle", "JSP"};
+		
+		try {
+			for(int i=0; i<subjectArr.length+1; i++) 
+				System.out.println(subjectArr[i]);				
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("배열의 인덱스 범위가 초과되었습니다.");		
+		}
+					
+	}
+
+}
