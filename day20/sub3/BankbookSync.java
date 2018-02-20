2e1b8438928da21a2edb882a2690d8c1ee055818
DerekYn <33822447+DerekYn@users.noreply.github.com>
2018-02-20 09:38:19 +0900

    Add files via upload

---
+++ day20/sub3/BankbookSync.java
@@ -0,0 +1,96 @@
+/*
+ 	=== 동기화(Synchronized) - 하나의 특정 메소드를 여러 스레드들이 순차적으로 줄을 서서 실행하는것 ===
+  	
+  		여러 스레드가 하나의 데이터를 공휴할 수 있다.
+  		공유 메커니즘은 성능 및 자원의 효율설에서는 좋지만 데이터 일관성에 있어서는 문제가 발생될 수 있다.
+  		여러 스레드가 하나의 데이터를 공유할 때 하나의 스레드가 해야하는 작업이 끝날때까지
+  		그 데이터는 다른 스레드에게 공유되지 않도록 해야 하는데,
+  		이것을 해결하는 방법으로 자바에서는 Synchronized 키워드를 사용하여 해결한다.
+  
+  		1. 동기화 메소드를 사용하는 방법
+  			
+  			접근 지정자 synchronized 리턴타입 메소드명() {
+  				----
+  				----
+  			}
+  			--> 메소드 선언시 synchronized 키워드를 지정하는 방법이다.
+  				이 방법은 메소드내의 모든 코드가 동기화 된다.
+  				따라서 특정 스레드가 이 메소드의 모든 내용을 모두 수행할 때까지 다른 스레드는 block 된다.
+  				
+  		2. 동기화 블럭을 사용하는 방법(추천)이다.
+			접근 지정자 리턴타입 메소드명() {
+				----
+					synchronized(공유객체) {
+						=====
+					}
+				----
+			}
+			--> 메소드내에서 synchronized 키워드 블럭을 사용하는 것이다.
+			이 방법은 메소드내의 모든 코드가 아닌 synchronized 블럭만 동기화 된다.
+			따라서 전체 메소드를 동기화 하는 방법보다 더 효율적이다.
+			
+		동일한 클래스의 같은 메소드를 동시에 여러 스레드가 호출해도
+		synchronized(동기화)에 의하여 순차적으로 스레드들이 실행된다.
+		에를 들어, 동일한 통장계좌 BankbookNoSync에 잔액이 100만원이 있을때
+		부부가 동시에 입금하는 스레드와 출금하는 스레드를 여러번 동시에 실행시켰을 경우
+		통장계좌에 입금과 출금 처리가 순차적으로 실행을 해서 기록을 해야 되는데,
+		그렇지 않은 경우 입. 출금 스레드의 실행순서를 우리가 알 수 없으므로,
+		통장계좌에 입금과 출금처리가 비 정상적으로 기록될 가능성이 있다.
+ */
+/*	=== 메소드에 동기화 처리를 한 경우  */
+
+package my.day20.sub3;
+
+public class BankbookSync { // 통장 클래스
+	
+	private String account; // 통장계좌번호
+	private long balance;	// 잔고액
+	
+
+	public String getAccount() {
+	
+		return account;
+	}
+	public void setAccount(String account) {
+		
+		this.account = account;
+	}
+	public long getBalance() {
+		
+		return balance;
+	}
+	public void setBalance(long balance) {
+		
+		this.balance = balance;
+	}
+	
+	
+	
+	// === 동기화를 한 입금 메소드. money 만큼 입금된다. ===
+	public synchronized void deposit(long money) {
+		
+		balance += money;
+		System.out.println("계좌번호 : " + account + " 입금액 => " + money + ", 입금 후 잔액 => " + balance);
+		
+	}
+	
+	// === 동기화를 한 출금 메소드. money 만큼 출금 된다.
+	public synchronized void withdraw(long money) {
+		
+		System.out.println("### 이것은 동기화 블럭밖의 내용입니다. ###");
+		
+		synchronized (this) { // 동기화 블럭 시작
+			if(balance - money < 0) {
+				
+				System.out.println("계좌번호 : " + account + "잔액부족 !!!  현재금액 => " + balance + ", 출금요청금액 => " + money);
+				
+				return;
+			}
+		}	// 동기화 블럭 끝
+		
+		balance -= money;
+		System.out.println("계좌번호 : " + account + " 출금액 => " + money + ", 출금 후 잔액 => " + balance);
+		
+	}
+	
+}
---
+++ day20/sub3/BankbookSyncMain.java
@@ -0,0 +1,74 @@
+package my.day20.sub3;
+
+public class BankbookSyncMain {
+
+	public static void main(String[] args) {
+		
+		// 통장 개설
+		BankbookSync myBankbook = new BankbookSync();
+		
+		myBankbook.setAccount("984-128-318784");
+		myBankbook.setBalance(10000);
+		
+		// *** 입금 스레드 구현 객체 10개 생성 ***
+		UpMoneyRunnable[] uprunArr = new UpMoneyRunnable[10];
+		Thread[] thrupArr = new Thread[10];
+		
+		for(int i=0; i<uprunArr.length; i++) {
+			
+			UpMoneyRunnable uprunnable = new UpMoneyRunnable();
+			
+			uprunnable.setBankbook(myBankbook);
+			// 여러 입금 스레드 구현 객체들이(10개) 특정 myBankbook 데이터 1개를 같이 사용함.
+			
+			uprunnable.setUpmoney(1000);	
+			// 입금액 1000원
+			
+			uprunArr[i] = uprunnable;
+			
+			thrupArr[i] = new Thread(uprunArr[i]);
+			
+		}
+		
+		
+		// *** 출금 스레드 구현 객체 10개 생성 ***
+		DownMoneyRunnable[] downrunArr = new DownMoneyRunnable[10];
+		Thread[] thrdownArr = new Thread[10];
+		
+		for(int i=0; i<downrunArr.length; i++) {
+			
+			DownMoneyRunnable downrunnable = new DownMoneyRunnable();
+			
+			downrunnable.setBankbook(myBankbook);
+			// 여러 출금 스레드 구현 객체들이(10개) 특정 myBankbook 데이터 1개를 같이 사용함.
+			
+			downrunnable.setDownmoney(1000);	
+			// 출금액 1000원
+			
+			downrunArr[i] = downrunnable;
+			
+			thrdownArr[i] = new Thread(downrunArr[i]);
+			
+		}
+		
+		for(int i=0; i<thrupArr.length; i++) {
+			thrupArr[i].start();	// 10개의 입금 스레드들을 작동시킴.
+			thrdownArr[i].start();	// 10개의 출금 스레드들을 작동시킴.
+			
+		}
+		
+		/*
+		 	이때 수행되는 스레드의 순서는 JVM이 결정지으므로 어떤 스레드가 먼저 수행되는지는 개발자는 알 수 없다.
+		 	그러므로 만약, 각각 해당 스레드의 run 메소드에서 수행되어지는 데이터가 동일한 객체(BankbookNoSync myBankbook 계좌번호 "984-128-318784")를
+		 	공유하는 내용이라면 실행되는 순서가 아주 중요한 것으로 발생될 수 있다.
+		 	이러한 순서를 지켜주기 위해서는 run 메소드에서 수행되어지는 메소드에 동기화(Synchronized - 순서를 지켜주는것) 처리를 반드시 해주어야 한다.
+		 	동기화 처리를 해주면 어떤 스레드 A가 동일한 객체 데이터 (BankbookNoSync myBankbook 계좌번호 "984-128-318784")에 접근하여
+		 	작업(deposit 또는 withdraw)을 처리할 동안에 다른 스레드 B 는 대기상태에 머물렀다가
+		 	어떤 스레드 A의 작업(deposit 또는 withdraw)이 끝나면 B 는 자신의 작업 (deposit 또는 withdraw)을 하는 방식으로 처리해준다.
+		 	하지만 어떤 스레드는 먼저 할지는 랜덤이므로 우리는 알 수가 없다.
+		 	입금 후 출금을 하든, 아니면 출금 후 입금을 하든 어떤것이든지 괜찮기 때문이다.
+		 */
+
+	}
+
+}
